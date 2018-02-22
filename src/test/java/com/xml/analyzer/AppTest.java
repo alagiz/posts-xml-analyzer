f111038f8628c9ff47dc9ab24e95a65d055dde70
artem.alagizov <artem.alagizov@gmail.com>
2018-02-22 14:39:55 +0100

    Changed main app unit test

--- src/test/java/com/xml/analyzer/AppTest.java
+++ src/test/java/com/xml/analyzer/AppTest.java
@@ -1,33 +1,20 @@
 package com.xml.analyzer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class AppTest {
+
+    @Test
+    public void applicationContextLoaded() {
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    public void applicationContextTest() {
+        App.main(new String[]{});
     }
 }
